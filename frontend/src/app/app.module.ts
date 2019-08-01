6693d7950ce9ad7a5a4754efe7355c6049d88a3a
narawichsaphimarn <NarawichSaphimarn@gmail.com>
2019-08-02 01:06:52 +0700

    edite Title component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TitleComponentComponent } from './title-component/title-component.component';
+
+import { RouterModule,Routes } from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+const appRoutes: Routes = [
+  {path: '',   redirectTo: '/Title',    pathMatch: 'full'},
+  { path: 'Title', component: TitleComponentComponent},
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitleComponentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes),
+    MatButtonModule, 
+    MatCheckboxModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
