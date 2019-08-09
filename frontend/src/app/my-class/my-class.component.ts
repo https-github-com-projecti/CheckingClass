a5749d96e2990f8f6ed5d46dc7a401a302853bbe
narawichsaphimarn <NarawichSaphimarn@gmail.com>
2019-08-09 13:58:59 +0700

    test

--- frontend/src/app/my-class/my-class.component.ts
+++ frontend/src/app/my-class/my-class.component.ts
@@ -5,11 +5,39 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './my-class.component.html',
   styleUrls: ['./my-class.component.css']
 })
+
 export class MyClassComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+    
+  }
 
   ngOnInit() {
+    // Get the modal
+    var modal = document.getElementById("myModal");
+                                
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+    
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+    
+    // When the user clicks the button, open the modal 
+    btn.onclick = function() {
+      modal.style.display = "block";
+    }
+    
+    // When the user clicks on <span> (x), close the modal
+    // span.onclick = function() {
+    //   modal.style.display = "none";
+    // }
+    
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
   }
 
 }
