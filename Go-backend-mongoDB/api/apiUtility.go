7982c48fd9e42f5a894202c64269d7bfb88c550e
iyongsin <44502117+iyongsin@users.noreply.github.com>
2019-10-01 02:33:20 +0700

    เงาผาน

--- Go-backend-mongoDB/api/apiUtility.go
+++ Go-backend-mongoDB/api/apiUtility.go
@@ -1,107 +1,107 @@
 package api
 
-import (
+// import (
 	
-	"Go-backend-mongoDB/repository"
+// 	"Go-backend-mongoDB/repository"
 
-	"Go-backend-mongoDB/model"
-	b64 "encoding/base64"
-	"github.com/skip2/go-qrcode"
-	"github.com/gin-gonic/gin"
-	"fmt"
+// 	"Go-backend-mongoDB/model"
+// 	b64 "encoding/base64"
+// 	"github.com/skip2/go-qrcode"
+// 	"github.com/gin-gonic/gin"
+// 	"fmt"
 	
-	"net/http"
-	_ "net/http"
-	"strconv"
+// 	"net/http"
+// 	_ "net/http"
+// 	"strconv"
 	
-	// "log"
-)	
-// type UtilityAPI struct {
-// 	AttendanceRepository repository.AttendanceRepository
-// 	UserRepository repository.UserRepository
-// 	SubjectRepository repository.SubjectRepository
-// 	StudentRepository repository.StudentRepository
-// }
+// 	// "log"
+// )	
+// // type UtilityAPI struct {
+// // 	AttendanceRepository repository.AttendanceRepository
+// // 	UserRepository repository.UserRepository
+// // 	SubjectRepository repository.SubjectRepository
+// // 	StudentRepository repository.StudentRepository
+// // }
 
-// // var newQrs []models.CreateQr
+// // // var newQrs []models.CreateQr
 
-func CreateBarcode(c *gin.Context) {
-	var png []byte
-	var p model.CreateQr
-	defer c.Request.Body.Close()
-	err := c.ShouldBindJSON(&p)
-	
-	str := strconv.Itoa(p.Pass)
-	var copyMyQr []model.CreateQr
-	var timeAuthens int = 0
-
-	// ดึงQrcodeออกมา เพื่อเช็คจำนวนการเช็คชื่อของแต่ละวิชา
-	// for _, sp := range newQrs {
-	// 	if p.Pass == sp.Pass {
-	// 		copyMyQr = append(copyMyQr, sp)
-	// 	}
-	// }
-
-	timeAuthens = len(copyMyQr) + 1
-	str2 := strconv.Itoa(timeAuthens)
-
-	png, _ = qrcode.Encode(p.Time+";"+p.User+";"+str+";"+str2, qrcode.Medium, 256)
-	
-	sEnc := b64.StdEncoding.EncodeToString([]byte(png))
-	sp := models.CreateQr{
-		Id:         len(newQrs) + 1,
-		Qrcode:     sEnc,
-		Time:       p.Time,
-		Pass:       p.Pass,
-		TimeAuthen: timeAuthens,
-	}
-
-	fmt.Println(sp)
-	newQrs = append(newQrs, sp)
-	c.JSON(http.StatusOK, "Success")
-}
-
-// func AllQr(c *gin.Context) {
+// func CreateBarcode(c *gin.Context) {
+// 	var png []byte
+// 	var p model.CreateQr
 // 	defer c.Request.Body.Close()
-// 	c.JSON(http.StatusOK, newQrs)
-// }
+// 	err := c.ShouldBindJSON(&p)
+	
+// 	str := strconv.Itoa(p.Pass)
+// 	var copyMyQr []model.CreateQr
+// 	var timeAuthens int = 0
 
-// func MyQr(c *gin.Context) {
-// 	flag.Parse()
-// 	defer c.Request.Body.Close()
-// 	pass := c.Param("pass")
-// 	i, _ := strconv.Atoi(pass)
-// 	var sp []models.CreateQr
-// 	for _, copy := range newQrs {
-// 		if copy.Pass == i {
-// 			sp = append(sp, copy)
-// 		}
-// 	}
-// 	c.JSON(http.StatusOK, sp)
-// }
+// 	// ดึงQrcodeออกมา เพื่อเช็คจำนวนการเช็คชื่อของแต่ละวิชา
+// 	// for _, sp := range newQrs {
+// 	// 	if p.Pass == sp.Pass {
+// 	// 		copyMyQr = append(copyMyQr, sp)
+// 	// 	}
+// 	// }
 
-// func GetshowQrCode(c *gin.Context) {
-// 	pass := c.Param("passOfcouse")
-// 	defer c.Request.Body.Close()
-// 	flag.Parse()
-// 	i, _ := strconv.Atoi(pass)
-// 	var sp []models.CreateQr
-// 	for _, copy := range newQrs {
-// 		if copy.Pass == i {
-// 			sp = append(sp, copy)
-// 		}
+// 	timeAuthens = len(copyMyQr) + 1
+// 	str2 := strconv.Itoa(timeAuthens)
+
+// 	png, _ = qrcode.Encode(p.Time+";"+p.User+";"+str+";"+str2, qrcode.Medium, 256)
+
+// 	sEnc := b64.StdEncoding.EncodeToString([]byte(png))
+// 	sp := models.CreateQr{
+// 		Id:         len(newQrs) + 1,
+// 		Qrcode:     sEnc,
+// 		Time:       p.Time,
+// 		Pass:       p.Pass,
+// 		TimeAuthen: timeAuthens,
 // 	}
 
-// 	len := len(sp)
 // 	fmt.Println(sp)
-// 	var sp2 []models.CreateQr
-// 	for _, copy := range sp {
-// 		if copy.TimeAuthen == len {
-// 			sp2 = append(sp2, copy)
-// 		}
-// 	}
-// 	c.JSON(http.StatusOK, sp2)
-// 	fmt.Println(len)
-// 	fmt.Println(sp2)
+// 	newQrs = append(newQrs, sp)
+// 	c.JSON(http.StatusOK, "Success")
 // }
+
+// // func AllQr(c *gin.Context) {
+// // 	defer c.Request.Body.Close()
+// // 	c.JSON(http.StatusOK, newQrs)
+// // }
+
+// // func MyQr(c *gin.Context) {
+// // 	flag.Parse()
+// // 	defer c.Request.Body.Close()
+// // 	pass := c.Param("pass")
+// // 	i, _ := strconv.Atoi(pass)
+// // 	var sp []models.CreateQr
+// // 	for _, copy := range newQrs {
+// // 		if copy.Pass == i {
+// // 			sp = append(sp, copy)
+// // 		}
+// // 	}
+// // 	c.JSON(http.StatusOK, sp)
+// // }
+
+// // func GetshowQrCode(c *gin.Context) {
+// // 	pass := c.Param("passOfcouse")
+// // 	defer c.Request.Body.Close()
+// // 	flag.Parse()
+// // 	i, _ := strconv.Atoi(pass)
+// // 	var sp []models.CreateQr
+// // 	for _, copy := range newQrs {
+// // 		if copy.Pass == i {
+// // 			sp = append(sp, copy)
+// // 		}
+// // 	}
+
+// // 	len := len(sp)
+// // 	fmt.Println(sp)
+// // 	var sp2 []models.CreateQr
+// // 	for _, copy := range sp {
+// // 		if copy.TimeAuthen == len {
+// // 			sp2 = append(sp2, copy)
+// // 		}
+// // 	}
+// // 	c.JSON(http.StatusOK, sp2)
+// // 	fmt.Println(len)
+// // 	fmt.Println(sp2)
+// // }
 
